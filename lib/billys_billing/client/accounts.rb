8dbee87540f54fd5e33e75c2c6393a0f2b34d021
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-08-16 14:20:08 -0600

    filter accounts

--- lib/billys_billing/client/accounts.rb
+++ lib/billys_billing/client/accounts.rb
@@ -2,8 +2,9 @@ require 'billys_billing/model/account'
 module BillysBilling
   class Client
     module Accounts
-      def accounts
-        get( 'accounts' )['accounts'].map { |a| Model::Account.new( a, client: self ) }
+      def accounts( filters = {} )
+        path = path_with_query_string( 'accounts', filters )
+        get( path )['accounts'].map { |a| Model::Account.new( a, client: self ) }
       end
 
       def account( id )
