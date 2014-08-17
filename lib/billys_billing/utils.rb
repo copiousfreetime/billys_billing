79613396b64922d6f5600ebfb86f6ad372488f11
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2014-08-17 14:44:10 -0600

    return the word if nothing happens

--- lib/billys_billing/utils.rb
+++ lib/billys_billing/utils.rb
@@ -2,7 +2,7 @@ module BillysBilling
   module Utils
     def snake_to_camel( word )
       word = word.to_s
-      return nil unless word.index('_')
+      return word unless word.index('_')
       leader, *parts = word.split('_')
       parts = parts.map { |p| p.capitalize }
       parts.unshift( leader )
